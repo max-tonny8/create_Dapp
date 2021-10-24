b725170c22f27c2a779bf1e81ac8e111d0d5c638
tonny0831 <tonnyjansen0831@gmail.com>
2021-10-24 14:01:23 +0100

    :rocket: add web3 global error handling

--- template/src/contracts/contracts.ts
+++ template/src/contracts/contracts.ts
@@ -1,20 +1,19 @@
-import { Contract } from "@ethersproject/contracts";
-import { useContractCall, useContractFunction } from "@usedapp/core";
+import { useContractCall } from "@usedapp/core";
 import { utils } from "ethers";
 
 import wethAbi from "./weth.json";
 import erc20Abi from "./erc20.json";
 import useGlobals from "../app/hooks/use-globals";
+import useContract from "../app/hooks/use-contract";
 
 export const useWrap = () => {
   const globals = useGlobals();
 
-  const { state: wrapState, send: wrap } = useContractFunction(
-    new Contract(globals.WETH, new utils.Interface(wethAbi)),
+  const { state: wrapState, send: wrap } = useContract(
+    globals.WETH,
+    wethAbi,
     "deposit",
-    {
-      transactionName: "Wrap",
-    }
+    "Wrap"
   );
   return { wrapState, wrap };
 };

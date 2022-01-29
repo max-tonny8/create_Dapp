c0234dc6d5e2393e9bc11e2c559b652d969c80fb
tonny0831 <tonnyjansen0831@gmail.com>
2022-01-29 15:42:55 +0000

    :rocket: add change network button

--- template/src/app/hooks/use-globals.ts
+++ template/src/app/hooks/use-globals.ts
@@ -1,20 +1,30 @@
 import { useEthers } from "@usedapp/core";
 import { useDispatch } from "react-redux";
-import { setError } from "../../state/errorSlice";
+import { clearError, ErrorButtonType, setError } from "../../state/errorSlice";
 import { ETH_GLOBALS, RINKEBY_GLOBALS } from "../globals";
 
 const useGlobals = () => {
   const dispatch = useDispatch();
   const { chainId } = useEthers();
 
-  if (chainId === 4) return RINKEBY_GLOBALS;
-  if (chainId === 1) return ETH_GLOBALS;
-  if (!chainId) return ETH_GLOBALS;
+  if (chainId === 4) {
+    dispatch(clearError());
+    return RINKEBY_GLOBALS;
+  }
+  if (chainId === 1) {
+    dispatch(clearError());
+    return ETH_GLOBALS;
+  }
+  if (!chainId) {
+    dispatch(clearError());
+    return ETH_GLOBALS;
+  }
 
   dispatch(
     setError({
       header: "Unsupported Network",
       subHeader: "Only Rinkey and Mainnet are supported",
+      button: ErrorButtonType.SWITCH_TO_MAINNET,
     })
   );
   return ETH_GLOBALS;

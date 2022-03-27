84fb6bda33e88529c05a1a98fc4b00bd8fca0a61
tonny0831 <tonnyjansen0831@gmail.com>
2022-03-27 20:43:22 +0100

    :rocket: add support for all testnets

--- template/src/components/WalletSelector.tsx
+++ template/src/components/WalletSelector.tsx
@@ -30,7 +30,10 @@ export const walletConnectConnector = new WalletConnectConnector({
   supportedChainIds,
   rpc: {
     1: `https://mainnet.infura.io/v3/${INFURA_ID}`,
+    3: `https://ropsten.infura.io/v3/${INFURA_ID}`,
     4: `https://rinkeby.infura.io/v3/${INFURA_ID}`,
+    5: `https://goerli.infura.io/v3/${INFURA_ID}`,
+    42: `https://kovan.infura.io/v3/${INFURA_ID}`,
   },
   qrcode: true,
 });
